cfd6eb78e8b47133443552cd77e0ffef61a6193f
单恬 <dantian@dantiandeMacBook.local>
2019-06-18 15:20:54 -0500

    Tian first commit

---
+++ tian.py
@@ -0,0 +1,15 @@
+# about me
+
+name = 'Tian'
+age = '23'
+hobbies = ['Watching movies', 'Traveling']
+
+if __name__ == '__main__':
+
+    print(f'My Name is {name}')
+    print(f'I am {age} years old')
+
+    print('My favorite things to do are...')
+    for i, h in enumerate(hobbies):
+
+        print(f'Hobby #{i + 1}  {h}')
